6c8be02d8cfc1169c53fd6b0327b4f07ac65ae5a
tmy_notebook <zb19851222@qq.com>
2019-09-02 11:03:10 +0800

    2 python :第5行打错导致测试用例不通过 错误原因：i 打成 1

--- 2.py
+++ 2.py
@@ -1,8 +1,30 @@
-def core()->int:
-    pass
+def core(value: list, isRalse: list, index: int, k: int) -> int:
+    result = 0
+    for i in range(k):
+        if (isRalse[index + i] == False):
+            result += value[index + 1]#这里马虎把i打成1，导致下边的测试用例不通过
+    return result
+
+
+import numpy as np
 
 if __name__ == '__main__':
-    n,k = map(int,input().strip().split(" "))
-    value = map(int,input().strip().split(" "))
-    isRalse = map(bool,input().strip().split(" "))
+    # n,k = map(int,input().strip().split(" "))
+    # value = [int(i) for i in input().strip().split(" ")]
+    # isRalse = [int(i)==1 for i in input().strip().split(" ")]
+    n, k = 10, 5
+    value = [int(i) for i in "6481 6127 4477 5436 7356 3137 1076 7182 8147 835".split(" ")]
+    isRalse = [int(i) == 1 for i in "1 0 1 0 1 1 0 0 0 1".strip().split(" ")]
+    rightSum = np.dot(np.array([int(i) for i in "6481 6127 4477 5436 7356 3137 1076 7182 8147 835".split(" ")]) , np.array(
+        [int(i) for i in "1 0 1 0 1 1 0 0 0 1".strip().split(" ")]).T)
+    sum = 0
+    maxNum = 0
     for i in range(n):
+        if (isRalse[i]):
+            sum += value[i]
+        if (i >= n - k + 1):
+            continue
+        temp = core(value, isRalse, i, k)
+        if (temp > maxNum):
+            maxNum = temp
+    print("%d" % (sum + maxNum))
