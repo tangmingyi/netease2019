035ee0d857546efe3fe791f2a57a4c7745ea2e35
tmy_notebook <zb19851222@qq.com>
2019-09-09 16:10:37 +0800

    7： 考点：大数组合数的求解（容易溢出当total>150）

--- 7.cpp
+++ 7.cpp
@@ -2,57 +2,69 @@
 // Created by zb198 on 2019/9/8.
 //
 #include <iostream>
-#include <vector>
 #include <string>
-void initVecter(std::vector<int >& a){
-    for(int i=0;i<a.size();i++){
-        a[i] = i;
+#include <math.h>
+//long long count(int n,int total){
+//    long long a=1,b=1;
+//    for(int i=0;i<n;i++){
+//        a *= (total-i);
+//    }
+//    for(int j=n;j>=1;j--){
+//        b *= j;
+//    }
+//    return a/b;
+//}
+bool equl(double a, double b){
+    return a-b < 0.001 && a-b > -0.001;
+}
+double count(int n,int total){
+    double a=0.0,b=0.0;
+    for(int i= 1;i<=total-n;i++){
+        b += log(double(i));
+    }
+    for(int j=n+1;j<=total;j++){
+        a += log(double(j));
+    }
+    return exp(a-b);
+
+}
+std::string core(double k,int n,int total){
+    if(n==0 || total==n){
+        std::string laststring;
+        for(int i=0;i<total;i++){
+            if(n==0) {
+                laststring += 'z';
+            } else{
+                laststring += 'a';
+            }
+        }
+        return laststring;
+    }
+    double temp = count(n-1,total-1);
+    if(k<temp||equl(k,temp)){
+        return 'a' + core(k,n-1,total-1);
+    } else{
+        return 'z' + core(k-temp,n,total-1);
     }
 }
 
-bool updateA(std::vector<int>& a, int end,int endIndex){
-    //能用index尽量就别用指针
-    a[end] += 1;
-    if(a[end]>endIndex){
-        a[end] -= 1;
-//        int change = end-1;
-        for(int change= end-1;change>=0;change--){
-            if(a[change+1]-a[change] != 1){
-                a[change] += 1;
-                for(int i=change+1;i<=end;i++){
-                    a[i] = a[i-1] + 1;
-                }
-                return true;
-            }
-        }
-        a[end] -=1;
-        return false;
-    }
-    return true;
-}
-void printResult(std::vector<int>& a, int n,int m){
-    std::string result(m+n,'z');
-    for(int i=0;i<n;i++){
-        result[a[i]] = 'a';
-    }
-    std::cout<<result<<std::endl;
+void test(){
+    int temp = count(2,6);
+    std::string result = core(6,2,4);
+    int c = 0;
 }
 int main(){
+    test();
     //总结：vector经过引用后尾指针失效（引用内和引用外都是），在引用内size()失效。
-    int n,m,k;
-    std::scanf("%d%d%d",&n,&m,&k);
-    std::vector<int > a(n);//a of index
-    initVecter(a);
-    int endIndex = n+m-1;
-    bool flagResult = true;
-    for(int i=1;i<k;i++){
-        flagResult = updateA(a,n-1,endIndex);
-        if(!flagResult){
-            break;
-        }
-    }
-    if(flagResult) {
-        printResult(a,n,m);
+    int n,m;
+    double k;
+    std::scanf("%d%d",&n,&m);
+    std::cin>>k;
+    double temp = count(n,n+m);
+    std::string resutl;
+    if (temp > k || equl(temp,k)){
+        resutl = core(k,n,m+n);
+        std::cout<<resutl<<std::endl;
     } else{
         std::printf("%d",-1);
     }
